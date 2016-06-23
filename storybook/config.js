e8fca2cb19c03d2a587f40f86397d142cf66aad0
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-23 12:50:45 +0530

    Add addon example

--- storybook/config.js
+++ storybook/config.js
@@ -1,6 +1,15 @@
-import { configure } from '@kadira/react-native-storybook';
+import React from 'react';
+import CenteredText from '../components/CenteredText';
+import { setAddon, configure } from '@kadira/react-native-storybook';
+
+setAddon({
+  addCenteredText(story, text) {
+    this.add(story, () => <CenteredText>{text}</CenteredText> );
+  }
+});
 
 configure(function () {
   require('./stories/CenteredText');
   require('./stories/SmallCenteredText');
+  require('./stories/UsingAddons');
 }, module);
---
+++ storybook/stories/UsingAddons.js
@@ -0,0 +1,6 @@
+import { storiesOf } from '@kadira/react-native-storybook';
+
+const stories = storiesOf('UsingAddons');
+
+stories.addCenteredText('First Story', 'First Story');
+stories.addCenteredText('Second Story', 'Second Story');
