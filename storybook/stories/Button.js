0686a280158f0b871d5c65180777d3252e3c2c78
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-23 14:34:54 +0530

    Use decorators

--- storybook/stories/Button.js
+++ storybook/stories/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { storiesOf } from '@kadira/react-native-storybook';
+import CenterView from '../../components/CenterView';
 import Button from '../../components/Button';
 
 const stories = storiesOf('Button');
+
+stories.addDecorator(story => (
+  <CenterView>{story()}</CenterView>
+));
 
 stories.add('default view', () => (
   <Button>
